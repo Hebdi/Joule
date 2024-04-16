e71a1b81c031987641732eaa43cf2053a3c10336
Hebdi <andyhess@hotmail.ch>
2024-04-16 02:09:45 +0200

    Intro & Build-Prep
    Intro-Text Animationen
    Props
    Train Animation
    etc...

--- Dome/Assets/Scripts/Player-Scripts/RespawnScript.cs
+++ Dome/Assets/Scripts/Player-Scripts/RespawnScript.cs
@@ -12,7 +12,7 @@ public class RespawnScript : MonoBehaviour
 
         if (Input.GetKey(KeyCode.F))
         {
-            transform.position = new Vector3(23.22f, 1.09f, 27.45f);
+            transform.position = new Vector3(-1581.469970703125f, 90.11299896240235f, -1186.8599853515625f);
         }
     }
 
@@ -21,8 +21,11 @@ public class RespawnScript : MonoBehaviour
         if (other.CompareTag("OffMap"))
         {
 
-            transform.position = new Vector3(23.22f, 1.09f, 27.45f);
+            transform.position = new Vector3(-2809.263916015625f, 90.97000122070313f, -2606.365966796875f);
 
         }
     }
 }
+//UnityEditor.TransformWorldPlacementJSON:{ "position":{ "x":-955.2474365234375,"y":0.01759999990463257,"z":-865.030517578125},"rotation":{ "x":0.0,"y":0.0,"z":0.0,"w":1.0},"scale":{ "x":1.0,"y":1.0,"z":1.0} } Behind Hill
+//UnityEditor.TransformWorldPlacementJSON:{"position":{"x":-1581.469970703125,"y":90.11299896240235,"z":-1186.8599853515625},"rotation":{"x":0.0,"y":0.0,"z":0.0,"w":1.0},"scale":{"x":1.0,"y":1.0,"z":1.0}} On DinerHill
+//UnityEditor.TransformWorldPlacementJSON:{"position":{"x":-2809.263916015625,"y":90.97000122070313,"z":-2606.365966796875},"rotation":{"x":0.0,"y":0.0,"z":0.0,"w":1.0},"scale":{"x":1.0,"y":1.0,"z":1.0}} On CanyonHill
