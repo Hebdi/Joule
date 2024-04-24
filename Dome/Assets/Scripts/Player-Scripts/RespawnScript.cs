687b01a99f33c22aaaa16e65d99bc1b92b40ddc6
Hebdi <andyhess@hotmail.ch>
2024-04-24 11:28:23 +0200

    Playtesting Scene | Minor Changes
    - Intro Aufbau
    - Intro Fade
    - Hinweis bei NPCs & Monument
    - Menü vereinfachung für Playtesting

--- Dome/Assets/Scripts/Player-Scripts/RespawnScript.cs
+++ Dome/Assets/Scripts/Player-Scripts/RespawnScript.cs
@@ -10,9 +10,9 @@ public class RespawnScript : MonoBehaviour
     void FixedUpdate()
     {
 
-        if (Input.GetKey(KeyCode.F))
+        if (Input.GetKey(KeyCode.G))
         {
-            transform.position = new Vector3(-1581.469970703125f, 90.11299896240235f, -1186.8599853515625f);
+            transform.position = new Vector3(-4833.9599609375f, 103.46600341796875f, -1806.7900390625f);
         }
     }
 
@@ -21,7 +21,7 @@ public class RespawnScript : MonoBehaviour
         if (other.CompareTag("OffMap"))
         {
 
-            transform.position = new Vector3(-2809.263916015625f, 90.97000122070313f, -2606.365966796875f);
+            transform.position = new Vector3(-3739.86865234375f, 14.270000457763672f, -1485.2315673828125f);
 
         }
     }
@@ -29,3 +29,5 @@ public class RespawnScript : MonoBehaviour
 //UnityEditor.TransformWorldPlacementJSON:{ "position":{ "x":-955.2474365234375,"y":0.01759999990463257,"z":-865.030517578125},"rotation":{ "x":0.0,"y":0.0,"z":0.0,"w":1.0},"scale":{ "x":1.0,"y":1.0,"z":1.0} } Behind Hill
 //UnityEditor.TransformWorldPlacementJSON:{"position":{"x":-1581.469970703125,"y":90.11299896240235,"z":-1186.8599853515625},"rotation":{"x":0.0,"y":0.0,"z":0.0,"w":1.0},"scale":{"x":1.0,"y":1.0,"z":1.0}} On DinerHill
 //UnityEditor.TransformWorldPlacementJSON:{"position":{"x":-2809.263916015625,"y":90.97000122070313,"z":-2606.365966796875},"rotation":{"x":0.0,"y":0.0,"z":0.0,"w":1.0},"scale":{"x":1.0,"y":1.0,"z":1.0}} On CanyonHill
+//UnityEditor.TransformWorldPlacementJSON:{"position":{"x":-3739.86865234375,"y":14.270000457763672,"z":-1485.2315673828125},"rotation":{"x":0.0,"y":0.9975530505180359,"z":0.0,"w":0.0699143037199974},"scale":{"x":1.0,"y":1.0,"z":1.0}} At Canyon - Playtesting Area
+//UnityEditor.TransformWorldPlacementJSON:{"position":{"x":-4833.9599609375,"y":103.46600341796875,"z":-1806.7900390625},"rotation":{"x":0.0,"y":0.0,"z":0.0,"w":1.0},"scale":{"x":1.0,"y":1.0,"z":1.0}} On Hill - Playtesting Area
