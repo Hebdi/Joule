25a5c53c23c714fcb30274ee578bdc568e9fe4df
Hebdi <andyhess@hotmail.ch>
2024-06-03 12:37:45 +0200

    UI / Wind / Location Trigger

--- Dome/Assets/Scripts/Audio/GroundCheckAudio.cs
+++ Dome/Assets/Scripts/Audio/GroundCheckAudio.cs
@@ -5,31 +5,22 @@ using UnityEngine;
 public class GroundCheckAudio : MonoBehaviour
 {
 
-    public bool grounded = false;
-    public float groundCheckDistance;
-    private float bufferCheckDistance = 0.1f; //slightly above 0
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
 
+    bool isGrounded;
 
 
     // Update is called once per frame
     void Update()
     {
-        groundCheckDistance = (GetComponent<CapsuleCollider>().height / 2) + bufferCheckDistance;
-        if (Input.GetKeyDown(KeyCode.W)&&grounded) //roll forwards
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
 
+        if (isGrounded && Input.GetKey(KeyCode.W))
         {
-            AudioManager.instance.PlayOneShot(FMODEvents.instance.playerRollSound, this.transform.position);
+            AudioManager.instance.PlayOneShot(FMODEvents.instance.playerRollSound, this.transform.position); //Play Roll Sound
         }
-
-        RaycastHit hit;
-        if (Physics.Raycast(transform.position, -transform.up, out hit, groundCheckDistance))
-        {
-            grounded = true;
-        } else
-        { //ray did not hit ground
-            grounded = false;
-        }
-
     }
 
 }
